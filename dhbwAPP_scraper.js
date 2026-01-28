8c6132004d2a998ab40cc15fb2ef341de4be8574
Chruisi <chruisi4@gmail.com>
2026-01-29 00:35:15 +0100

    Scraper funktioniert soweit, es fehlen noch an anderer Stelle die Aufrufe sobald man den Kurs auswählt und falls man den Stundenplan anzeigen will und die zugehörige JSON schon zu alt ist

--- dhbwAPP_scraper.js
+++ dhbwAPP_scraper.js
@@ -1,52 +1,144 @@
 /*
 Scraper für die Webseite dhbw.app
 
-Zieht sich die Termine und Zeiten von der Seite www.dhbw.app und speichert sie in einer JSON im data Ordner ab.
+Zieht sich die Termine und Zeiten von der Seite www.dhbw.app und speichert sie in einer JSON im timetables Ordner ab.
+
+Solo-Testen des Scrapers:
+    node dhbwAPP_scraper.js (geht nur wenn sessionCourse manuell gesetzt ist)
 
 Class für die einzelnen Tage:    "mt-3 divide-gray-500 divide-y w-11/12 sm:w-5/6 md:2-3/4 lg:w-3/6 mx-auto"
 Class für die einzelnen Termine: "rounded-xl shadow-2xl py-2 px-4 mt-4 bg-opacity-85 bg-zinc"
 Class für Termin Name:           "text-zinc-300 select-none text-lg md:text-xl" (ist ein span)
-Class für die Zeit:              "flex-grow text-zinc-300 truncate" (ist ein span), gibt es dreimal pro termin (Datum: 26.01.2026, Uhrzeit:08.00 - 12.00, Raum: H213b,Hörsaal)
+
+Datum:
+    Class Calender-Icon: "tabler-icon tabler-icon-calendar-event text-zinc-300 flex-none my-auto h-5 w-5"
+    Inhalt: "flex-grow text-zinc-300 truncate" (ist das Span direkt dahinter)
+    Format: XX.XX.XXXX
+
+Uhrzeit:
+    Class Zeit-Icon: "tabler-icon tabler-icon-clock-hour-8 text-zinc-300 flex-none my-auto h-5 w-5"
+    Inhalt: "flex-grow text-zinc-300 truncate" (ist das Span direkt dahinter)
+    Format: XX.XX - XX.XX (XX.XX %)
+
+Raum:
+    Class Raum-Icon: "tabler-icon tabler-icon-home text-zinc-300 flex-none my-auto h-5 w-5"
+    Inhalt: "flex-grow text-zinc-300 truncate" (ist das Span direkt dahinter)
+    Format: H213b,Hörsaal
+
+EXTRAS:
+
+DHBW Standort bei speziellen Events wie Blutspenden:
+    Class Map-Pin-Icon: "tabler-icon tabler-icon-map-pin text-zinc-300 flex-none my-auto h-5 w-5"
+    Inhalt: "flex-grow text-zinc-300 truncate" (ist das Span direkt dahinter)
+    Format: FN
+
+INFO:
+    Class Info-Icon: "tabler-icon tabler-icon-info-square-rounded text-zinc-300 flex-none my-auto h-5 w-5"
+    Inhalt: "flex-grow text-zinc-300 line-clamp-3" (ist das Span direkt dahinter)
+    Format: Beliebiger Text
 */
+
+const sessionCourse = "TIT24";
+
+//Hier wird der ausgewählte Kurs ausgelesen und die URL zusammengesetzt
+//const sessionCourse = window.sessionStorage.getItem('kurs');
+//console.log("Ausgelesener Kurs aus Session: " + sessionCourse);
+
+//Standort Präfix bestimmen Friedrichshafen (FN) oder Ravensburg (RV)
+let Kurs = "";
+if (sessionCourse.startsWith("T")) {
+    Kurs = "FN-" + sessionCourse;
+}else if (sessionCourse.startsWith("W")) {
+    Kurs = "RV-" + sessionCourse;
+}
+
+const url = `https://dhbw.app/c/${Kurs}`;
 
 import puppeteer from "puppeteer";
 
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
-await page.goto("https://dhbw.app/c/FN-TIT24", {
+await page.goto(url, {
     waitUntil: "networkidle2"
 });
-await page.waitForSelector(".flex-grow.text-zinc-300.truncate"); // wartet auf die Terminzeiten
-//await page.waitForTimeout(1000); // 1 Sekunde extra
 
-const data = await page.evaluate(() => {
+    page.on('console', msg => {
+        // msg.text() enthält den Text, der im Browser geloggt wurde
+        console.log('BROWSER LOG:', msg.text());
+    });
+
+// wartet dass die Terminzeiten gerendert sind
+await page.waitForSelector(".flex-grow.text-zinc-300.truncate");
+
+const scrapingResult = await page.evaluate(() => {
+    //Sucht sich alle Tage heraus und speichert sie als Array
     const days = document.querySelectorAll(".mt-3.divide-gray-500");
+
+    //Jeder Tag wird einzeln verarbeitet
     return Array.from(days).map(day => {
+
+        //Erstes Element im Tag ist der Ausgeschriebene Wochentag und Datum
         const header = day.firstElementChild;
-        const headerText = header.innerText.trim();
-        const weekday = headerText.split(" - ")[0];
+        const weekday = header.innerText.trim().split(" - ")[0]; //Wochentag
+        const date = header.innerText.trim().split(" - ")[1];    //Datum
 
+        //Nach dem Header kommt ein Element das alle Termine enthält
         const dayContent = header.nextElementSibling;
-        const appointmentArray = Array.from(dayContent.querySelectorAll(".rounded-xl.shadow-2xl"));
-        
-        const appointments = appointmentArray.map(appointment => {
-            const name = appointment.querySelector(".text-zinc-300.select-none")?.innerText.trim() || "Terminname nicht vorhanden";
 
-            const appointmentData = appointment.querySelectorAll(".flex-grow.text-zinc-300.truncate");
-            const time = appointmentData[1]?.innerText.trim() || "Zeit nicht vorhanden";
-            const startTime = time.split(" - ")[0].replace(".", ":") || "Startzeit nicht vorhanden";
-            const endTime = time.split(" - ")[1].replace(".", ":") || "Endzeit nicht vorhanden";
-            const date = appointmentData[0]?.innerText.trim() || "datum nicht vorhanden";
-            const location = appointmentData[2]?.innerText.trim() || "raum nicht vorhanden";
-            
-            return {name, date, startTime, endTime, location};
+        //Aus dem Tag werden alle Termine über die Klasse rausgesucht
+        const appointmentArray = Array.from(dayContent.querySelectorAll(".rounded-xl.shadow-2xl"));
+
+
+        //Jeder Termin wird einzeln verarbeitet
+        const appointments = appointmentArray.map(appointment => {
+
+            //Holt sich den Namen des Termins
+            const name = appointment.querySelector(".text-zinc-300.select-none")?.innerText.trim() || "Termin ohne Name";
+
+            //DEPRECATED: Holt sich das Datum des Termins (wird aktuell nicht genutzt da im Header des Tages enthalten)
+            //Datum des Termins
+            //const dateIcon = appointment.querySelector(".tabler-icon-calendar-event");
+            //const dateElement = dateIcon ? dateIcon.nextElementSibling : null;
+            //const date = dateElement?.innerText.trim() || "Datum nicht vorhanden";
+
+            //Uhrzeiten des Termins
+            const timeIcon = appointment.querySelector(".tabler-icon-clock-hour-8");
+            const timeElement = timeIcon ? timeIcon.nextElementSibling : null;
+            const time = timeElement?.innerText.trim() || "Zeit nicht vorhanden";
+            let startTime = "Startzeit nicht verfügbar";
+            let endTime = "Endzeit nicht verfügbar";
+            if (time !== "Zeit nicht vorhanden") {
+                const time_array = time.split(" - ");
+                startTime = time_array[0];
+                const endTime_dummy = time_array[1];
+                //Falls die Vorlesung gerade läuft muss die Prozentzahl abgeschnitten werden
+                const spaceIndex = endTime_dummy.indexOf(" ");
+                endTime = spaceIndex === -1 ? endTime_dummy : endTime_dummy.slice(0, spaceIndex);
+            }
+
+            //Raum des Termins
+            const locationIcon = appointment.querySelector(".tabler-icon-home");
+            const locationElemnt = locationIcon ? locationIcon.nextElementSibling : null;
+            const location = locationElemnt?.innerText.trim() || "Raum nicht angegeben";
+
+            //Spetialtermin Ort
+            const locationExtraIcon = appointment.querySelector(".tabler-icon-map-pin");
+            const locationExtraElemnt = locationExtraIcon ? locationExtraIcon.nextElementSibling : null;
+            const locationExtra = locationExtraElemnt?.innerText.trim() || null;
+
+            //Info Text
+            const infoIcon = appointment.querySelector(".tabler-icon-info-square-rounded");
+            const infoElement = infoIcon ? infoIcon.nextElementSibling : null;
+            const info = infoElement?.innerText.trim() || null;
+
+            return {name, startTime, endTime, location, locationExtra, info};
         });
 
-        return {weekday, appointments};
+        return {weekday, date, appointments};
     });
 });
 
 await browser.close();
 
 import fs from "fs";
-fs.writeFileSync("./data/dhbwAPP_data.json", JSON.stringify(data, null, 2), "utf-8");
+fs.writeFileSync(`./data/timetables/${Kurs}.json`, JSON.stringify(scrapingResult, null, 2), "utf-8");
